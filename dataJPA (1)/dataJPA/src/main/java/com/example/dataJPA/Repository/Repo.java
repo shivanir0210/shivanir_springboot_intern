fa10e4a0bc2c3136d951d7a97377d3016d1bda5f
shivanir0210 <shivanirasappan@gmail.com>
2026-06-03 07:25:01 +0530

    Delete dataJPA (1) directory

--- dataJPA (1)/dataJPA/src/main/java/com/example/dataJPA/Repository/Repo.java
+++
@@ -1,7 +0,0 @@
-package com.example.dataJPA.Repository;
-
-import com.example.dataJPA.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repo extends JpaRepository <Student,Integer>{
-}
--- dataJPA (1)/eduhub-backend/src/main/java/com/eduhub/eduhub_backend/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-
-package com.eduhub.eduhub_backend.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource,String field,String error) {
-        super(String.format("%s not found with %s:%s", resource, field,error));
-    }
-
-}
